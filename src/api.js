29cb62c63c9a883e1159e1d0d1afd4bddad8033e
SvitRia <129522432+SvitRia@users.noreply.github.com>
2023-09-06 21:11:13 -0400

    update load more, checks

--- src/api.js
+++ src/api.js
@@ -1,11 +1,8 @@
-import Notiflix from 'notiflix';
+//import Notiflix from 'notiflix';
 import axios from "axios";
-//axios.defaults.baseURL = "https://pixabay.com/api/";
-//axios.defaults.headers.common["x-api-key"] = "39130911-8039e4f23f6b3aae8a4a0d71c";
 
     const BAS_URL = "https://pixabay.com/api/";
     const API_KEY = "39130911-8039e4f23f6b3aae8a4a0d71c";
-
 
 export async function fetchList(querySearch, page) {
     const params = new URLSearchParams({
@@ -17,40 +14,35 @@ export async function fetchList(querySearch, page) {
         page: `${page}`,
         per_page: "40",
       });
-
-    try {
+ 
       const response = await axios.get(`${ BAS_URL }?&${ params }`);
       console.dir(response);
-      if (!response.data.hits.length) {throw new Error()};
+     
       return response.data;
-    } catch (error) {
-        Notiflix.Notify.warning(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
     }
-}
   
 export function createMarkUp(results) {
-    const markUp = results.map((result) => `<div class="photo-card">
-<a class="gallery__item" href="${result.largeImageURL}"><img class="gallery__image" src="${result.webformatURL}" alt="${result.tags}" loading="lazy"/>
-<div class="info">
-  <p class="info-item">
-  <b>Likes:</b><br />${result.likes}
-  </p>
-  <p class="info-item">
-    <b>Views:</b><br />${result.views}
-  </p>
-  <p class="info-item">
-    <b>Comments:</b><br />${result.comments}
-  </p>
-  <p class="info-item">
-    <b>Downloads:</b><br />${result.downloads}
-  </p>
-</div>
-</a>
-</div>`).join("");
+    const markUp = results.map((result) => 
+    `<div class="photo-card">
+       <a class="gallery__item" href="${result.largeImageURL}"><img class="gallery__image" src="${result.webformatURL}" alt="${result.tags}" loading="lazy"/>
+          <div class="info">
+              <p class="info-item">
+                <b>Likes:</b><br />${result.likes}
+              </p>
+              <p class="info-item">
+                <b>Views:</b><br />${result.views}
+              </p>
+              <p class="info-item">
+                <b>Comments:</b><br />${result.comments}
+              </p>
+              <p class="info-item">
+                <b>Downloads:</b><br />${result.downloads}
+              </p>
+         </div>
+     </a>
+ </div>`).join("");
 
- return markUp
+ return markUp;
 };
   
     
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+//import axios from "axios";
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
@@ -7,33 +7,40 @@ import { fetchList, createMarkUp } from "./api";
 
 const {searchForm, gallerySelector, searchBtn, searchQuery, loadBtn} = refs
 
-//axios.defaults.headers.common["x-api-key"] = "39130911-8039e4f23f6b3aae8a4a0d71c";
-
 let page = 1;
 let querySearch = "";
 let totalPages = 1;
 const perPage = 40;
 
-const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
 
 loadBtn.classList.add("not-visible");
-searchForm.addEventListener("submit", onFirstList)
-
-async function  onFirstList(evt) {
+searchForm.addEventListener("submit", onSearchFormSubmit)
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: "alt", overlayOpacity: 0.5 });
+async function  onSearchFormSubmit(evt) {
     evt.preventDefault();
     gallerySelector.innerHTML = "";
     page = 1;
     querySearch = evt.currentTarget.searchQuery.value;
-    console.log(querySearch);
+    if(!querySearch.trim()) {
+        loadBtn.classList.add("not-visible");
+        return
+    }
     const result = await fetchList(querySearch, page);
-    console.log(result);
-    const { total, totalHits, hits} = result;
-    searchQuery.value ="";
+    const { totalHits, hits} = result;
+    searchQuery.value = "";
+    if(totalHits === 0) {
+        Notiflix.Notify.warning(`Sorry, there are no images matching your search query. Please try again.`)
+        return;
+    }
     Notiflix.Notify.success(`Hooray! We found ${totalHits} images!`);
+    if(totalHits < perPage) {
+        return;
+    }
     totalPages = Math.ceil(totalHits / perPage);
     gallerySelector.innerHTML = createMarkUp(hits);
+    lightbox.refresh();
     loadBtn.classList.remove("not-visible");
-    
 }
 
 loadBtn.addEventListener("click", onMorePhoto)
@@ -41,22 +48,18 @@ loadBtn.addEventListener("click", onMorePhoto)
 async function  onMorePhoto(evt) {
     evt.preventDefault();
     page += 1;
-    lightbox.refresh();
-    console.log(totalPages);
-
-    if(page > totalPages) {
+   
+    if( page === totalPages ) {
         loadBtn.classList.add("not-visible");
         Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
-        return;
     }
+
     const result = await fetchList(querySearch, page);
-    const {totalHits, hits} = result;
+   
+    const { hits} = result;
     console.log(hits);
-    Notiflix.Notify.success(`Hooray! We found ${totalHits} images!`);
     
     gallerySelector.insertAdjacentHTML ("beforeend",createMarkUp(hits));
-
-    
-
+    lightbox.refresh();
 }
 
